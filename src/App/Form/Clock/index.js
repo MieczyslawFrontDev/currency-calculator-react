008b025c2d54a64bf8268def40bd7b567cc9bd41
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-07 11:49:26 +0100

    new function formatDate added

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -1,25 +1,29 @@
 import "./style.css";
 import { useState, useEffect } from "react";
 
+const formatDate = (date) => date.toLocaleTimeString(undefined, {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
 export const Clock = () => {
 
-  const [date, setDate] = useState(new Date());
+const [date, setDate] = useState(new Date());
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
+useEffect(() => {
+  const intervalId = setInterval(() => {
+    setDate(new Date())
+  }, 1000);
+  return () => clearInterval(intervalId);
+}, []);
 
   return (
     <p className="clock">
-      {`Dzisiaj jest: ${date.toLocaleTimeString("pl", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-      })}`}
+      Dzisiaj jest
+      {" "}
+      {formatDate(date)}
     </p>
   );
 };
