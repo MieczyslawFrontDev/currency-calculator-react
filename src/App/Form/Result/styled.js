a5272128b4b17544280edfa4c6de9b66f57b8738
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-12 10:20:39 +0100

    styled-components added

---
+++ src/App/Form/Result/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Paragraph = styled.p`
+  font-size: 24px;
+  text-align: center;
+`;
+
+export const StyledResult = styled.p`
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  margin: 0 auto;
+`;
